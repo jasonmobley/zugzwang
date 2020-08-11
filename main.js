5392d145c4fecd91b5bc72ea3f6fdbe456abc269
Jason Mobley <jason@mobleyweb.com>
2020-08-10 21:51:07 -0400

    More stuff

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-class App {
+export class App {
   constructor() {
   }
 
   init(props) {
-    this._main = props.main;
-    this._resetButton = props.resetButton ?? null;
+    this._main = props.main ?? '.main';
     this._selectedColor = props.selectedColor ?? null;
-    this._whiteColorButton = props.whiteColorButton ?? null;
-    this._blackColorButton = props.blackColorButton ?? null;
+    this._whiteColorButton = props.whiteColorButton ?? '#btn-color-white';
+    this._blackColorButton = props.blackColorButton ?? '#btn-color-black';
+    this._resetButton = props.resetButton ?? '#btn-reset';
 
     if (this._resetButton) {
-      this._resetButton.addEventListener('click', () => { this.setColor(null); });
+      this.el(this._resetButton).addEventListener('click', () => { this.setColor(null); });
     }
 
     if (this._whiteColorButton) {
-      this._whiteColorButton.addEventListener('click', () => { this.setColor('white'); });
+      this.el(this._whiteColorButton).addEventListener('click', () => { this.setColor('white'); });
     }
     if (this._blackColorButton) {
-      this._blackColorButton.addEventListener('click', () => { this.setColor('black'); });
+      this.el(this._blackColorButton).addEventListener('click', () => { this.setColor('black'); });
     }
   }
 
@@ -33,17 +33,19 @@ class App {
     return this._blackColorButton;
   }
 
+  el(selector) {
+    return document.querySelector(selector);
+  }
+
   setColor(color) {
     if (this._selectedColor === 'white' || this._selectedColor === 'black') {
-      this._main.classList.remove(this._selectedColor);
+      this.el(this._main).classList.remove(this._selectedColor);
     }
     if (color === 'white' || color === 'black') {
       this._selectedColor = color;
-      this._main.classList.add(this._selectedColor);
+      this.el(this._main).classList.add(this._selectedColor);
     } else {
       this._selectedColor = null;
     }
   }
 }
-
-export App;
